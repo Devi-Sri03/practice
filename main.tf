8feee153721c723295f6469696b4268f1cb85d21
Devi-Sri03 <devidhatla@gmail.com>
2024-12-05 14:05:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 
 resource "aws_instance" "dev" {
     ami = "ami-0614680123427b75e"
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
     key_name = "darling"
     tags = {
       Name = "test"
